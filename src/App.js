b69ed870dd19f58a9f672b9593eb0bf4a2c3e741
PaugelS20 <95315799+PaugelS20@users.noreply.github.com>
2022-01-05 16:32:32 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,15 +9,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-         <p>Hello World!</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+         <p>Paugel"'"s Website</p>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer"> Learn React</a>
       </header>
     </div>
   );
