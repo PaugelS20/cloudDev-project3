3b05cab8b9e92d81c09324920a5fa431f075b470
PaugelS20 <95315799+PaugelS20@users.noreply.github.com>
2022-01-18 11:54:49 -0600

    App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+/*import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -9,7 +9,32 @@ function App() {
            <p>
              Edit <code>src/App.js</code> and save to reload.
            </p>
-            <h2>Paugels Website</h2>
-       <header/>
+            /*<h2>Paugels Website</h2>*/
+       </header>*/
+
+export default App;
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
 
 export default App;
