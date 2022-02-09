5ec2bd476266cd4c479dec9252a31ab62d3f6867
PaugelS20 <95315799+PaugelS20@users.noreply.github.com>
2022-02-08 23:18:48 -0600

    App.js

--- src/App.js
+++ src/App.js
@@ -1,36 +1,14 @@
-// import logo from './logo.svg';
-// import './App.css';
-// import'./index.html';
-// import'./index.html';
-// import'./index.html';
-// import'./index.html';
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-/*import './Master.css';*/
-function App() {
-  return (
+/*import logo from './logo.svg';
+import './App.css';
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo"/>
+        <p Edit <code>src/App.js</code> and save to reload.</p>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">Learn React</a>
+      </header>
+     </div>*/
+function App() { return (
 <div className="App">
-
-//  <head>
-//     <meta charset="utf-8"/>
-//     <meta name="viewport" content="width=device-width, initial-scale=1"/>
-//     <link rel="icon" type="favicon" href="favicon.ico"/>
-//     <title>Home</title>
-// </head>
 <div className="container">
   <body>
       <header>
@@ -45,14 +23,14 @@ function App() {
 <main>
     <h1 id="mainContent" id="storeheader">Welcome to the Store</h1>
     <section>
-        <img id="cloneVinyl"src="./CloneVinyl.png" alt="clone trooper vinyl"/>
+        <img id="cloneVinyl" src="images/CloneVinyl.png" alt="clone trooper vinyl"/>
         <p className="width">This is our music store we have everything from cds, vinyl, mp3s and more! There is so much to choose from. We also have everything priced according to value or use. I sincerely hope you enjoy our store and leave us a review.</p>
-        <img src=".images/tripple.png" alt=""/>
+        <img src="images/tripple.png" alt=""/>
     </section>
 
     <section>
         <p className="width">You can find out more about us on our About us page. There we go indepth about who we are, our mission and what makes us groove.Like M*A*S*H is my personal groove it's my favorite TV show!</p>
-        <img id="mash" src=".images/MASH.png" alt="mash actors wayne rogers and alan alda"/>
+        <img id="mash" src="images/MASH.png" alt="mash actors wayne rogers and alan alda"/>
     </section>
     </main>
   </body>
@@ -69,12 +47,12 @@ function App() {
     <p>Mon-Fri: 9am-8pm</p> 
     <p>Sat-Sun: 10am-7pm</p>
 
-    <h3>Add us here</h3>
-        <a href="https://www.instagram.com/"><img className="InstaHandle" src=".images/instagram.svg" alt="Instagram Logo" target="blank"/></a>
-        <a href="https://www.facebook.com/"><img className="InstaHandle" src=".images/facebook.svg" alt="Facebook Logo" target="blank"/></a>
-        <a href="https://www.twitter.com/"><img className="InstaHandle" src=".images/twitter.svg" alt="Twitter Logo" target="blank"/></a>
-        <a href="https://www.youtube.com/"><img className="InstaHandle" src=".images/youtube.svg" alt="Youtube Logo" target="blank"/></a>
- <p>Sam Paugel 2021©</p>
+    <h3>Add Me here</h3>
+        <a href="https://www.instagram.com/"><img className="InstaHandle" src="images/instagram.svg" alt="Instagram Logo" target="blank"/></a>
+        <a href="https://www.facebook.com/"><img className="InstaHandle" src="images/facebook.svg" alt="Facebook Logo" target="blank"/></a>
+        <a href="https://www.twitter.com/"><img className="InstaHandle" src="images/twitter.svg" alt="Twitter Logo" target="blank"/></a>
+        <a href="https://www.youtube.com/"><img className="InstaHandle" src="images/youtube.svg" alt="Youtube Logo" target="blank"/></a>
+ <i>Sam Paugel 2021©</i>
 </footer>
 
 
