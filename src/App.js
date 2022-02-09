cf028ad953d5763794492bfa3032c34c3249b7f5
PaugelS20 <95315799+PaugelS20@users.noreply.github.com>
2022-02-09 11:03:13 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ function App() { return (
 <div className="container">
   <body>
       <header>
-        <img id="Logo" src="images/Logo.png" alt="music shop logo"/>
+        <img id="Logo" src="./Logo.png" alt="Music-shop-logo"/>
               <nav aria-label="Main Menu">
                   <ul aria-label="Main Menu" role="menubar">
             
@@ -16,12 +16,12 @@ function App() { return (
     <section>
         <img id="cloneVinyl" src="./CloneVinyl.png" alt="clone trooper vinyl"/>
         <p className="width">This is our music store we have everything from cds, vinyl, mp3s and more! There is so much to choose from. We also have everything priced according to value or use. I sincerely hope you enjoy our store and leave us a review.</p>
-        <img src="images/tripple.png" alt=""/>
+        <img src="./tripple.png" alt="Tripple pictures"/>
     </section>
 
     <section>
         <p className="width">You can find out more about us on our About us page. There we go indepth about who we are, our mission and what makes us groove.Like M*A*S*H is my personal groove it's my favorite TV show!</p>
-        <img id="mash" src="../MASH.png" alt="mash actors wayne rogers and alan alda"/>
+        <img id="mash" src="./MASH.png" alt="mash actors wayne rogers and alan alda"/>
     </section>
     </main>
   </body>
@@ -39,10 +39,10 @@ function App() { return (
     <p>Sat-Sun: 10am-7pm</p>
 
     <h3>Add Me here</h3>
-        <a href="https://www.instagram.com/"><img className="InstaHandle" src="images/instagram.svg" alt="Instagram Logo" target="blank"/></a>
-        <a href="https://www.facebook.com/"><img className="InstaHandle" src="images/facebook.svg" alt="Facebook Logo" target="blank"/></a>
-        <a href="https://www.twitter.com/"><img className="InstaHandle" src="images/twitter.svg" alt="Twitter Logo" target="blank"/></a>
-        <a href="https://www.youtube.com/"><img className="InstaHandle" src="images/youtube.svg" alt="Youtube Logo" target="blank"/></a>
+        <a href="https://www.instagram.com/"><img className="InstaHandle" src="./instagram.svg" alt="Instagram Logo" target="blank"/></a>
+        <a href="https://www.facebook.com/"><img className="InstaHandle" src="./facebook.svg" alt="Facebook Logo" target="blank"/></a>
+        <a href="https://www.twitter.com/"><img className="InstaHandle" src="./twitter.svg" alt="Twitter Logo" target="blank"/></a>
+        <a href="https://www.youtube.com/"><img className="InstaHandle" src="./youtube.svg" alt="Youtube Logo" target="blank"/></a>
  <i>Sam Paugel 2021©</i>
 </footer>
 
