3c47100de9cd7410145b9586f09494c4d3e6e533
PaugelS20 <95315799+PaugelS20@users.noreply.github.com>
2022-02-08 23:42:40 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() { return (
         <img id="Logo" src="images/Logo.png" alt="music shop logo"/>
               <nav aria-label="Main Menu">
                   <ul aria-label="Main Menu" role="menubar">
-                      /*<li><a href=index.html>Home</a></li>*/
+                     
                       <li><a href=Products.html>Products</a></li>
                       <li><a href=Members.html>Members</a></li>
                       <li><a href=AboutUs.html>About Us</a></li>
