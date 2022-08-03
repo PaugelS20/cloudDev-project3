3ea6952d50b3443f0b59380e142aea3693fb2282
Sam Paugel <shpaugel@madisoncollege.edu>
2022-08-03 02:02:56 -0500

    go

--- src/App.js
+++ src/App.js
@@ -4,13 +4,7 @@ import './Master.css';
 /*import {BrowserRouter as Router} from 'react-router-dom';*/
 function App() { return (
 <div className="App">
-    <div className="import Navbar from './Navbar.js';
-import React from 'react';
-import './Master.css';
-/*import {BrowserRouter as Router} from 'react-router-dom';*/
-function App() { return (
-<div className="App">
-    <div className="container">
+    <div id="container">
 
 
         <header id="mainContent">
@@ -21,44 +15,7 @@ function App() { return (
     <body>
             <main>
 
-            // <h1><div class="text">S</div>
-            // <div class="text">a</div>
-            // <div class="text">m</div>
-            // <div class="text">P</div>
-            // <div class="text">a</div>
-            // <div class="text">u</div>
-            // <div class="text">g</div>
-            // <div class="text">e</div>
-            // <div class="text">l</div></h1>
-
-            </main>
-            <footer>
-                <h2>Contact Me</h2>
-                <p>Madison College 1701 Wright St, Madison, WI 53704</p>
-                <p><a href="tel:+16085144097">(608)-514-4097</a></p>
-                <p><a href="mailto:shpaugel@madisoncollege.edu">shpaugel@madisoncollege.edu</a></p>
-
-                <h2>Add Me here</h2>
-                <a href="https://www.instagram.com/paugs17/"><img className="socialMedia" src="./instagram.svg" alt="Instagram Logo" target="blank" /></a>
-                <a href="https://www.facebook.com/sam.paugel.3/"><img className="socialMedia" src="./facebook.svg" alt="Facebook Logo" target="blank" /></a>
-                <i>Developed by Sam Paugel 2022©</i>
-            </footer>
-        </body>
-        <a href="#mainContent" className="skipLink"></a>
-    </div>
-</div>
-);} export default App;
-grid-container">
-
-    <body>
-        <header>
-            <h1 id="mainContent">Sam Paugel</h1>
-            <Navbar/>
-        </header>
-
-            <main>
-
-            <h1><div class="text">S</div>
+            /*<h1><div class="text">S</div>
             <div class="text">a</div>
             <div class="text">m</div>
             <div class="text">P</div>
@@ -66,7 +23,7 @@ grid-container">
             <div class="text">u</div>
             <div class="text">g</div>
             <div class="text">e</div>
-            <div class="text">l</div></h1>
+            <div class="text">l</div></h1>*/
 
             </main>
             <footer>
@@ -84,4 +41,5 @@ grid-container">
         <a href="#mainContent" className="skipLink"></a>
     </div>
 </div>
-);} export default App;
+);
+} export default App;
