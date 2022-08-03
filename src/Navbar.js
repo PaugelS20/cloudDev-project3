f9d478f124d58c5194a646e4f33c4a46ca76ca29
Sam Paugel <shpaugel@madisoncollege.edu>
2022-08-02 22:41:27 -0500

    Fixed?

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,19 +1,40 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import './Master.css';
-// import Dropdown from './Dropdown';
+import React, {useState, useEffect} from 'react'
+import './Navbar.css'
 
-function Navbar() {
-    // const [click, setClick] = useState(false)
-    //
-    // const handleClick = () => setClick(!click)
-    //
-    // return (
-    //         <nav className='Navbar'>
-    //             <Link to='/' className='Navbar-logo'></Link>
-    //             <div className='menu-icon' onClick={handleClick}>
-    //                 <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
-    //             </div>
-    //         </nav>
-    // );
+export default function Navbar() {
+  const [toggleMenu, setToggleMenu] = useState(false)
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth)
+
+
+  const toggleNav = () => {
+    setToggleMenu(!toggleMenu)
+  }
+
+  useEffect(() => {
+
+    const changeWidth = () => {
+      setScreenWidth(window.innerWidth);
+    }
+
+    window.addEventListener('resize', changeWidth)
+
+    return () => {
+        window.removeEventListener('resize', changeWidth)
+    }
+
+  }, [])
+
+  return (
+    <nav>
+      {(toggleMenu || screenWidth > 500) && (
+      <ul className="list">
+      <li className="items">Home</li>
+      <li className="items">Services</li>
+      <li className="items">Contact</li>
+    </ul>
+      )}
+
+      <button onClick={toggleNav} className="btn">BTN</button>
+    </nav>
+  )
 }
--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,23 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+    // import React, { useState } from 'react';
+    // import { Link } from 'react-router-dom';
+    // import './Master.css';
+    // import Dropdown from './Dropdown';
+
+    // function Navbar() {
+//}
+// const [click, setClick] = useState(false)
+//
+// const handleClick = () => setClick(!click)
+//
+// return (
+//         <nav className='Navbar'>
+//             <Link to='/' className='Navbar-logo'></Link>
+//             <div className='menu-icon' onClick={handleClick}>
+//                 <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+//             </div>
+//         </nav>
+// );
