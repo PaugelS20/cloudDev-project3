f8a4b59a17c8229d2475b36afcc2a3834866688c
Sam Paugel <shpaugel@madisoncollege.edu>
2022-08-03 01:54:16 -0500

    fixed 1

--- src/Navbar.js
+++ src/Navbar.js
@@ -27,14 +27,27 @@ export default function Navbar() {
   return (
     <nav>
       {(toggleMenu || screenWidth > 500) && (
-      <ul className="list">
-      <li className="items">Home</li>
-      <li className="items">Services</li>
-      <li className="items">Contact</li>
-    </ul>
+        <ul className="main-menu">
+        <a id="logo" rel="about" href="www.sampaugel.com">
+        <img src="Images/logo.png" alt="img goes here"/>
+        <span>Paugel</span></a>
+            <li className="items"><a href="about.html">About</a></li>
+            <li className="items"><a href="skils.html">Skills</a></li>
+            <li className="items"><a href="contact.html">Contact</a></li>
+        </ul>
       )}
-
       <button onClick={toggleNav} className="btn">BTN</button>
     </nav>
+
   )
 }
+/*
+<nav className="main-menu">
+{(toggleMenu || screenWidth > 500) && (
+      <a className="items" href="index.html">About</a>
+      //<a className="items" href="skills.html">Skills</a>
+      //<a className="items" href="yourmom.html">Contact</a>
+
+)}
+<button onClick={toggleNav} className="btn">BTN</button>
+</nav>*/
