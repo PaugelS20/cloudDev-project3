1cc7d35943df56967e696d0ae80e49300b745508
Sam Paugel <shpaugel@madisoncollege.edu>
2022-09-16 22:00:55 -0500

    Fixes on fixes

--- src/App.js
+++ src/App.js
@@ -1,45 +1,63 @@
-import Navbar from './Navbar.js';
-import React from 'react';
-import './Master.css';
+/* Sam Paugel 2022© */
+import Navbar from "./Navbar.js";
+import React from "react";
+import "./Master.css";
 /*import {BrowserRouter as Router} from 'react-router-dom';*/
-function App() { return (
-<div className="App">
-    <div id="container">
+function App() {
+  return (
+    <div className="App">
+      {/* <div id="container"> */}
+      <header id="mainContent"></header>
+      <body>
+        <Navbar />
 
-
-        <header id="mainContent">
-            <menu>
-                <Navbar/>
-            </menu>
-        </header>
-    <body>
-            <main>
-
-            /*<h1><div class="text">S</div>
-            <div class="text">a</div>
-            <div class="text">m</div>
-            <div class="text">P</div>
-            <div class="text">a</div>
-            <div class="text">u</div>
-            <div class="text">g</div>
-            <div class="text">e</div>
-            <div class="text">l</div></h1>*/
-
-            </main>
-            <footer>
-                <h2>Contact Me</h2>
-                <p>Madison College 1701 Wright St, Madison, WI 53704</p>
-                <p><a href="tel:+16085144097">(608)-514-4097</a></p>
-                <p><a href="mailto:shpaugel@madisoncollege.edu">shpaugel@madisoncollege.edu</a></p>
-
-                <h2>Add Me here</h2>
-                <a href="https://www.instagram.com/paugs17/"><img className="socialMedia" src="./instagram.svg" alt="Instagram Logo" target="blank" /></a>
-                <a href="https://www.facebook.com/sam.paugel.3/"><img className="socialMedia" src="./facebook.svg" alt="Facebook Logo" target="blank" /></a>
-                <i>Developed by Sam Paugel 2022©</i>
-            </footer>
-        </body>
-        <a href="#mainContent" className="skipLink"></a>
+        <div>
+          <div className="text-area">
+            <h2 aria-label="Me Myself and I">Me Myself and I</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Blanditiis illum reiciendis animi ad voluptatum. Exercitationem
+              error, quae dolorem repellat quibusdam enim voluptatem et saepe
+              quam minima officiis recusandae culpa cumque. Lorem ipsum dolor,
+              sit amet consectetur adipisicing elit. Corporis officia soluta
+              cumque excepturi maiores itaque earum inventore architecto, eaque
+              eveniet illum perferendis beatae in quis dolorum harum voluptatum
+              molestiae ipsa!
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Blanditiis illum reiciendis animi ad voluptatum. Exercitationem
+              error, quae dolorem repellat quibusdam enim voluptatem et saepe
+              quam minima officiis recusandae culpa cumque. Lorem ipsum dolor,
+              sit amet consectetur adipisicing elit. Corporis officia soluta
+              cumque excepturi maiores itaque earum inventore architecto, eaque
+              eveniet illum perferendis beatae in quis dolorum harum voluptatum
+              molestiae ipsa!
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Blanditiis illum reiciendis animi ad voluptatum. Exercitationem
+              error, quae dolorem repellat quibusdam enim voluptatem et saepe
+              quam minima officiis recusandae culpa cumque. Lorem ipsum dolor,
+              sit amet consectetur adipisicing elit. Corporis officia soluta
+              cumque excepturi maiores itaque earum inventore architecto, eaque
+              eveniet illum perferendis beatae in quis dolorum harum voluptatum
+              molestiae ipsa!
+            </p>
+          </div>
+        </div>
+      </body>
+      <a href="#mainContent" className="skipLink"></a>
     </div>
-</div>
-);
-} export default App;
+  );
+}
+export default App;
+// <h1><div class="text">S</div>
+// <div class="text">a</div>
+// <div class="text">m</div>
+// <div class="text">P</div>
+// <div class="text">a</div>
+// <div class="text">u</div>
+// <div class="text">g</div>
+// <div class="text">e</div>
+// <div class="text">l</div></h1>
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,53 +1,108 @@
-import React, {useState, useEffect} from 'react'
-import './Navbar.css'
+/* Sam Paugel 2022© */
+import React, { useState, useEffect } from "react";
+import "./Navbar.css";
+import { BsFacebook, BsInstagram, BsLinkedin } from "react-icons/bs";
+import { SiIndeed } from "react-icons/si";
+import { IconContext } from "react-icons";
+
 
 export default function Navbar() {
-  const [toggleMenu, setToggleMenu] = useState(false)
-  const [screenWidth, setScreenWidth] = useState(window.innerWidth)
-
-
+  const [toggleMenu, setToggleMenu] = useState(false);
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
   const toggleNav = () => {
-    setToggleMenu(!toggleMenu)
-  }
-
+    setToggleMenu(!toggleMenu);
+  };
   useEffect(() => {
-
     const changeWidth = () => {
       setScreenWidth(window.innerWidth);
-    }
-
-    window.addEventListener('resize', changeWidth)
-
+    };
+    window.addEventListener("resize", changeWidth);
     return () => {
-        window.removeEventListener('resize', changeWidth)
-    }
-
-  }, [])
+      window.removeEventListener("resize", changeWidth);
+    };
+  }, []);
 
   return (
-    <nav>
-      {(toggleMenu || screenWidth > 500) && (
-        <ul className="main-menu">
-        <a id="logo" rel="about" href="www.sampaugel.com">
-        <img src="Images/logo.png" alt="img goes here"/>
-        <span>Paugel</span></a>
-            <li className="items"><a href="about.html">About</a></li>
-            <li className="items"><a href="skils.html">Skills</a></li>
-            <li className="items"><a href="contact.html">Contact</a></li>
-        </ul>
-      )}
-      <button onClick={toggleNav} className="btn">BTN</button>
-    </nav>
+    <menu className="main-menu">
+      {/* (toggleNav || screenWidth 768px)
+      { */}
+      <a onClick={toggleMenu} class="" rel="home"></a>
+      <div className="main-menu__top">
+        <a class="logoLink" href="www.sampaugel.com">
+          <img id="paugel-logo" src="images/logo.png" alt="Paugel Logo" />
+          <span id="logoText">Paugel</span>
+        </a>
+        <span>Front End Developer</span>
+      </div>
+      <nav className="main-menu_nav">
+        <a href="about.html">About</a>
+        <a href="skills.html">Skills</a>
+        <a href="contact.html">Contact</a>
+      </nav>
 
-  )
+      <ul className="social">
+      <IconContext.Provider value={{ className: "socialLinks" }}>
+        <li><a href="https://www.linkedin.com/in/sam-paugel-884829232/"><BsLinkedin/></a></li>
+        <li><a href=""><SiIndeed/></a></li>
+        <li><a href="https://www.instagram.com/paugs17/"><BsInstagram/></a></li>
+        <li><a href="https://www.facebook.com/sam.paugel.3/"><BsFacebook/></a></li>
+        </IconContext.Provider>
+      </ul>
+    </menu>
+  );
 }
-/*
-<nav className="main-menu">
-{(toggleMenu || screenWidth > 500) && (
-      <a className="items" href="index.html">About</a>
-      //<a className="items" href="skills.html">Skills</a>
-      //<a className="items" href="yourmom.html">Contact</a>
+//
+//<button onClick={toggleNav} className="btn"><img src="images/menu-icon.svg" alt="Menu Icon"/></button>
+{
+  /* <button onClick={toggleMenu} class="btn" >
+            <BsList />
+          </button> */
+}
+{
+  /* <img id="menu-icon" src="images/menu-icon.svg" alt="Menu Icon" /> */
+}
 
-)}
-<button onClick={toggleNav} className="btn">BTN</button>
-</nav>*/
+// import React, {useState, useEffect} from 'react'
+// import './Navbar.css'
+
+// export default function Navbar() {
+//   const [toggleMenu, setToggleMenu] = useState(false)
+//   const [screenWidth, setScreenWidth] = useState(window.innerWidth)
+
+
+//   const toggleNav = () => {
+//     setToggleMenu(!toggleMenu)
+//   }
+
+//   useEffect(() => {
+
+//     const changeWidth = () => {
+//       setScreenWidth(window.innerWidth);
+//     }
+
+//     window.addEventListener('resize', changeWidth)
+
+//     return () => {
+//         window.removeEventListener('resize', changeWidth)
+//     }
+
+//   }, [])
+
+//   return (
+//     <nav>
+//       {(toggleMenu || screenWidth > 500) && (
+//         <ul className="main-menu">
+//         <a id="logo" rel="about" href="www.sampaugel.com">
+//         <img src="Images/logo.png" alt="img goes here"/>
+//         <span>Paugel</span></a>
+//             <li className="items"><a href="about.html">About</a></li>
+//             <li className="items"><a href="skils.html">Skills</a></li>
+//             <li className="items"><a href="contact.html">Contact</a></li>
+//         </ul>
+//       )}
+//       <button onClick={toggleNav} className="btn">BTN</button>
+//     </nav>
+
+//   )
+// }
+
