5f087ad987ea3b7de1f68790eea17ab28c1c9d57
Sam Paugel <shpaugel@madisoncollege.edu>
2022-10-17 14:20:02 -0500

    added animate to make bounce effect on letters

--- src/App.js
+++ src/App.js
@@ -3,18 +3,25 @@ import Navbar from "./Navbar.js";
 import React from "react";
 import "./Master.css";
 import Menu from "./Menu.js";
-import mainContent from "./index.html";
+import html from "./index.html"
 /*import {BrowserRouter as Router} from 'react-router-dom';*/
 function App() {
-	return (
-		
-		<div className="App">
-		
-				<Navbar />
-				<Menu />
-				<mainContent/>
-		</div>
-	);
+  return (
+    <div className="App">
+      <header id="mainContent"></header>
+      <body>
+        <Navbar />
+        <Menu />
+          <div className="text-area">
+            <h2 aria-label="Hi,I'm Sam, a Frontend Developer">Hi,I'm Sam, a Frontend Developer</h2>
+            <p>
+              
+            </p>
+      </div>
+      </body>
+      <a href="#mainContent" className="skipLink"></a>
+    </div>
+  );
 }
 export default App;
 // <h1><div class="text">S</div>
