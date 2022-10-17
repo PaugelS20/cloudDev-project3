d35c07d4b8eba9af59008cf1e15c9e8e13624a99
Sam Paugel <shpaugel@madisoncollege.edu>
2022-10-17 13:52:57 -0500

    Deleted lorem changed Added new araia label text

--- src/App.js
+++ src/App.js
@@ -13,16 +13,9 @@ function App() {
         <Menu />
         <div>
           <div className="text-area">
-            <h2 aria-label="Me Myself and I">Hi,I'm Sam, a Frontend Developer</h2>
+            <h2 aria-label="Hi,I'm Sam, a Frontend Developer">Hi,I'm Sam, a Frontend Developer</h2>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Blanditiis illum reiciendis animi ad voluptatum. Exercitationem
-              error, quae dolorem repellat quibusdam enim voluptatem et saepe
-              quam minima officiis recusandae culpa cumque. Lorem ipsum dolor,
-              sit amet consectetur adipisicing elit. Corporis officia soluta
-              cumque excepturi maiores itaque earum inventore architecto, eaque
-              eveniet illum perferendis beatae in quis dolorum harum voluptatum
-              molestiae ipsa!
+              
             </p>
           </div>
         </div>
