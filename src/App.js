c2beb08ba3d94b2ad716ce36c75f2d9522389987
Sam Paugel <shpaugel@madisoncollege.edu>
2022-10-19 00:29:59 -0500

    hope cross my fingers and toes this works please

--- src/App.js
+++ src/App.js
@@ -36,6 +36,29 @@ function App() {
 	);
 }
 export default App;
+
+// /* Sam Paugel 2022© */
+// import Navbar from "./Navbar.js";
+// import React from "react";
+// import "./Master.css";
+// import Menu from "./Menu.js";
+// /*import {BrowserRouter as Router} from 'react-router-dom';*/
+// function App() {
+//   return (
+//     <div className="App">
+//       <header id="mainContent"></header>
+//       <body>
+//         <Navbar />
+//         <Menu />
+//           <div className="text-area">
+//            <h2 aria-label="Hi, I'm Sam, a Frontend Developer" class="animate__animated animate__bounce">Hi, I'm Sam, a Frontend Developer</h2>
+//       </div>
+//       </body>
+//       <a id="mainContentLink" href="#mainContent" className="skipLink">main content</a>
+//     </div>
+//   );
+// }
+// export default App;
 // <h1><div class="text">S</div>
 // <div class="text">a</div>
 // <div class="text">m</div>
