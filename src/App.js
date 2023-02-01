c80feb51c07af8b1ea0477716abbd50abffa8d6a
Sam Paugel <shpaugel@madisoncollege.edu>
2023-02-01 17:30:27 -0600

    Starting from scratch tired of all the crap

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 /* Sam Paugel 2022© */
 import React from "react";
-import Menu from "./Menu.js";
-import Navbar from "./Navbar.js";
-/* Core CSS required for Ionic components to work properly */
-import '@ionic/react/css/core.css';
+// import Navbar from "./Navbar.js";
+// import '@ionic/react/css/core.css';
+// import { IonButton, IonDatetime } from '@ionic/react';
 import "./Master.css";
 
 const App = () => {
   return (
+
     <div className="App">
+
+
+{/* 
+    <>
+      <IonDatetime></IonDatetime>
+      <IonButton fill="clear">Start</IonButton>
+    </>
+
+
+
+
+
       <header id="mainContent"></header>
       <body>
         <Navbar />
         {/* <Menu /> */}
-          <div className="text-area">
+          {/* <div className="text-area">
            <h2 aria-label="Hi, I'm Sam, a Frontend Developer" class="animate__animated animate__bounce">Hi, I'm Sam, a Frontend Developer</h2>
            <p>I am currently learning Avd CSS And JS Frameworks. In JS Frameworks I have been learning how to use Angular which so far is a very easy and fun language to learn!</p>
       </div>
       </body>
-      <a id="mainContentLink" href="#mainContent" className="skipLink">main content</a>
+      <a id="mainContentLink" href="#mainContent" className="skipLink">main content</a> */} 
     </div>
   );
 }
