ba3c26ce2700db01be9b0add549b9b310cd6bc5f
Sam Paugel <shpaugel@madisoncollege.edu>
2023-02-01 16:45:59 -0600

    updating old react things

--- src/Menu.js
+++ src/Menu.js
@@ -1,16 +1,16 @@
-import React, { useState } from "react";
-import Navbar from "./Navbar";
-import { Squash as Hamburger } from "hamburger-react";
-import "./Navbar.css";
-import "./Menu.css";
+// learimport React, { useState } from "react";
+// import Navbar from "./Navbar";
+// import { Squash as Hamburger } from "hamburger-react";
+// import "./Navbar.css";
+// import "./Menu.css";
 
-export default function Menu() {
-  const [open, setShow] = useState(false);
-  return (
-    <div className="hamburger">
-        {open?<Navbar />:setShow}
-        <Hamburger onClick={() => setShow(open) }/>
-    </div>
+// export default function Menu() {
+//   const [menuOpen, ] = useState(false);
+//   return (
+//     <div className="hamburger">
+//         {open?<Navbar />:setShow}
+//         <Hamburger onClick={() => setShow(open) }/>
+//     </div>
     
-  );
-}
+//   );
+// }
