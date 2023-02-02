fe2558eccd4566a4df8f61950aca7faadc069ac9
Sam Paugel <shpaugel@madisoncollege.edu>
2023-02-01 19:42:41 -0600

    Adding bootstrap better than nothing

---
+++ src/App.js
@@ -0,0 +1,29 @@
+/* Sam Paugel 2022© */
+import React from "react";
+import "./Master.css";
+
+
+setupIonicReact();
+
+const App = () => {
+  return (
+    <div className="App">
+      <h2 aria-label="Hi, I'm Sam, a Frontend Developer" class="animate__animated animate__bounce">Hi, I'm Sam, a Frontend Developer</h2>
+      <p>I am currently learning Avd CSS And JS Frameworks. In JS Frameworks I have been learning how to use Angular which so far is a very easy and fun language to learn!</p>
+    </div>
+    
+  );
+}
+export default App;
+
+{/* 
+  <header id="mainContent"></header>
+  <body>
+    <Navbar />
+    {/* <Menu /> */}
+      {/* <div className="text-area">
+        
+  </div>
+  </body>
+  <a id="mainContentLink" href="#mainContent" className="skipLink">main content</a> */} 
+
