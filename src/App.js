4885a56b2ef7e3082fbe91b7f252bbe5c176748e
Sam Paugel <shpaugel@madisoncollege.edu>
2023-02-01 20:06:46 -0600

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@
 import React from "react";
 import "./Master.css";
 
-
-setupIonicReact();
-
 const App = () => {
   return (
     <div className="App">
+
+      <div class="card" style="width: 18rem;">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+
       <h2 aria-label="Hi, I'm Sam, a Frontend Developer" class="animate__animated animate__bounce">Hi, I'm Sam, a Frontend Developer</h2>
       <p>I am currently learning Avd CSS And JS Frameworks. In JS Frameworks I have been learning how to use Angular which so far is a very easy and fun language to learn!</p>
     </div>
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import './index.css';
@@ -9,8 +9,7 @@ import reportWebVitals from './reportWebVitals';
 ReactDOM.render(
     <React.StrictMode>
         <App />
-    </React.StrictMode>, 
-    document.getElementById('root'));
+    </React.StrictMode>,document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
