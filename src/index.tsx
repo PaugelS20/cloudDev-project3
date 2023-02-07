416e2096235f34f44583f537d44e59a1934f8d58
Sam Paugel <shpaugel@madisoncollege.edu>
2023-02-07 17:07:53 -0600

    still unsure how ionic works

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
+// Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+// Bootstrap Bundle JS
+import "bootstrap/dist/js/bootstrap.bundle.min";
 // import reportWebVitals from './reportWebVitals';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root')); root.render( <App /> );
 
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -5,21 +5,21 @@ import { IonCard,
   IonCardSubtitle, 
   IonCardTitle
 } from '@ionic/react'
-import {BScard} from 'bootstrap';
+
 
 
 export const Home = () => {
   return (
-      <IonCard>
+      <IonCard className='--ion-background-color'>
           <IonCardHeader>
-            <IonCardTitle>Home Page</IonCardTitle>
-            <IonCardSubtitle>Welcome</IonCardSubtitle>
+            <IonCardTitle></IonCardTitle>
+            <IonCardSubtitle></IonCardSubtitle>
           </IonCardHeader>
 
           <IonCardContent>
             <h2 aria-label="Hi, I'm Sam, a Frontend Developer">Hi, I'm Sam, a Frontend Developer</h2>
-            <p>I am currently learning Avd CSS And JS Frameworks. In JS Frameworks I have been learning how to use Angular which so far is a very easy and fun language to learn!</p>      
+            <p>I am currently learning Avd CSS And JS Frameworks. In JS Frameworks I have been learning how to use Angular which so far is a very easy and fun language to learn!</p>
           </IonCardContent>
-        </IonCard>    
+      </IonCard>
   );
 }
