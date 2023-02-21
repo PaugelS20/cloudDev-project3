11ac93f3e5be928b745392efa62bd449fdd0ecf8
Sam Paugel <shpaugel@madisoncollege.edu>
2023-02-21 14:08:45 -0600

    moved master css import to allow other imports first

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { Home, About, Contact, Navbar } from "./pages"
-import "./Master.css";
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
 
@@ -19,8 +18,9 @@ import '@ionic/react/css/text-transformation.css';
 import '@ionic/react/css/flex-utils.css';
 import '@ionic/react/css/display.css';
 import { setupIonicReact } from '@ionic/react';
-
 setupIonicReact();
+import "./Master.css";
+
 
 const App = () => {
   return (
