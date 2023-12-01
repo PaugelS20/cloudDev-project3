8d867f0b6bb0db59876f04bac892b77ec12bcd28
Sam Paugel <95315799+PaugelS20@users.noreply.github.com>
2023-12-01 12:32:37 -0600

    Still figuring out menu toggle Navbar.tsx

--- src/pages/Navbar.tsx
+++ src/pages/Navbar.tsx
@@ -24,11 +24,13 @@ export const Navbar = () => {
             
             <IonContent className="ion-padding">
                 <ul className='navbar-nav me-auto mb-2 mb-lg-0'>
-		<IonMenuToggle>
+		
                   <IonButton fill="clear">
+		    <IonMenuToggle>
                       <Link className="navLink" to='/'>Home</Link>
+		    </IonMenuToggle>
                   </IonButton>
-		</IonMenuToggle>
+		
 
                   <IonButton fill="clear">
                       <Link className="navLink" to='/About'>About</Link>
@@ -51,7 +53,7 @@ export const Navbar = () => {
 		    
                   	<IonButtons slot="start">
                     		<nav className="navbar-small-devices">
-                      		   <IonMenuToggle className='menuButton'></IonMenuToggle>
+                      		   <IonMenuButton className='menuButton'></IonMenuButton>
                     		</nav>
 				<IonRow>
 					<IonCol>
