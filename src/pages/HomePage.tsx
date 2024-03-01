461464a3d9311cab808b9941fcb2168796f0fc67
Sam Paugel <shpaugel@madisoncollege.edu>
2024-02-29 22:55:01 -0600

    Lots of new updates!
    Copy right year was updated to the current year, old content that was using up space was removed, the star wars image was reuploaded hope that works, MATC year was updated to 2024, New desciption in main text was updated to current info so far, New icon for school was added for effect, thats about it hope it all works!

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,4 @@
-/* Sam Paugel 2023© */
+/* Sam Paugel 2024© */
 import React from 'react';
 
 import { 
@@ -11,40 +11,39 @@ import {
   	IonRow,
 	IonCol,
 	IonImg,
-	IonContent,
-	IonIcon,	
+	IonIcon,
 } from '@ionic/react'
-import { logoGithub } from 'ionicons/icons';
-// import { logIn } from 'ionicons/icons';
-// IonItem,IonInput,IonLabel,IonButton,IonIcon
+import { logoGithub, school } from 'ionicons/icons';
+// import { Link } from 'react-router-dom';
+
 
 export const Home = () => {
   return (
-   <>
-			
+   <>		
     <IonGrid>
 		<IonRow>
 			<IonCol className='spacing'>
 				<IonCard>
-				{/* <img className='starWarsImg' src="../images/Star-Destroyer.png" alt="Star wars star destryoer img"/> */}
+				
+					<IonImg className='starWarsImg' src="../images/Star-Destroyer.png" alt="Star wars star destryoer img"></IonImg>
+				
 					<IonCardHeader>
 						<IonCardTitle aria-label="intro-title">
 							Hi, I'm Sam, a Frontend Developer
 						</IonCardTitle>
+						
 						<IonCardSubtitle>
-							MATC 2023
+							MATC 2024
 						</IonCardSubtitle>
 					</IonCardHeader>
 
 					<IonCardContent>
-					
+						<p aria-label="intro-text">
+							Currently I'm looking for full-time entry-level positions as a Front End Developer. My graduation date is May of this year 2024 <IonIcon icon={school}></IonIcon>. Joining any team to create websites with passion and meaning. Please <a href="/Contact">contact</a> me with any questions you might have.
+						</p>
 
-					<p aria-label="intro-text">I am currently learning Advanced Front End, UIUX, 
-					and Serverless Cloud with React. I am currently learning how to 
-					use API's to make AWS and GitHub pages builds.</p>
-
-					<p>Here's my  <a href="https://github.com/PaugelS20">GitHub Profile<IonIcon icon={logoGithub}></IonIcon>
-					</a> to see some of the things I'm working on right now.</p>
+						<p>Here's my  <a href="https://github.com/PaugelS20">GitHub Profile<IonIcon icon={logoGithub}></IonIcon></a>
+						 to see some of the things I'm working on right now.</p>
 					</IonCardContent>
 				</IonCard>
 			</IonCol>
