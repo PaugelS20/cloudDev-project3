304f2b3ac150a1fb1886ce40d1fe629fe1072a5c
Sam Paugel <shpaugel@madisoncollege.edu>
2024-07-23 20:30:42 -0500

    Bootstrap was finally removed completely
    was outdated and interfering with the ionic css framework so it was removed

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 // Bootstrap CSS
-import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
 // Bootstrap Bundle JS
-import "bootstrap/dist/js/bootstrap.bundle.min";
+// import "bootstrap/dist/js/bootstrap.bundle.min";
 // import reportWebVitals from './reportWebVitals';
-import './index.css';
+import './Master.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root')); root.render( <App /> );
 
