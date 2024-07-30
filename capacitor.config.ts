396632cfd31a71646a1b555eb8e402ac2fb72fb1
Sam Paugel <shpaugel@madisoncollege.edu>
2024-07-30 16:01:40 -0500

    Added capacitor
    I wanted to try it out and see what capabilities it's offering

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.project4',
+  appName: 'project4',
+  webDir: 'build'
+};
+
+export default config;
