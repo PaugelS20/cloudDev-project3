90978b1630c45488d260444ba50cc0c7da788bc6
Sam Paugel <shpaugel@madisoncollege.edu>
2024-12-10 01:28:53 -0600

    Finally after months of work scroll is fixed
    I feel so stupid now for not realizing all I had to do like I did in the Farkle project is literally just add IonPage and IonContent around the whole thing in the App.tsx to surround all the elements no matter which page is being used man it feels good to be back in the drivers seat

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,47 @@
 /* Sam Paugel 2024© */
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import { Home, About, Contact, Navbar } from "./pages"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home, About, Contact, Navbar } from "./pages";
+import { IonPage, IonContent } from "@ionic/react";
 import "./Master.css";
 /* Core CSS required for Ionic components to work properly */
-import '@ionic/react/css/core.css';
+import "@ionic/react/css/core.css";
 
 /* Basic CSS for apps built with Ionic */
-import '@ionic/react/css/normalize.css';
-import '@ionic/react/css/structure.css'; // Remove if nothing is visible
-import '@ionic/react/css/typography.css';
+import "@ionic/react/css/normalize.css";
+import "@ionic/react/css/structure.css"; // Remove if nothing is visible
+import "@ionic/react/css/typography.css";
 
 /* Optional CSS utils that can be commented out */
-import '@ionic/react/css/padding.css';
-import '@ionic/react/css/float-elements.css';
-import '@ionic/react/css/text-alignment.css';
-import '@ionic/react/css/text-transformation.css';
-import '@ionic/react/css/flex-utils.css';
-import '@ionic/react/css/display.css';
-import { setupIonicReact } from '@ionic/react';
+import "@ionic/react/css/padding.css";
+import "@ionic/react/css/float-elements.css";
+import "@ionic/react/css/text-alignment.css";
+import "@ionic/react/css/text-transformation.css";
+import "@ionic/react/css/flex-utils.css";
+import "@ionic/react/css/display.css";
+import { setupIonicReact } from "@ionic/react";
 setupIonicReact();
 
-
-
 const App = () => {
-  return (
-    <div className="App">
-    <BrowserRouter>
-    <Navbar/>
-      <Routes>
-            <Route path="/" element={ <Home/> }/>
-            <Route path="/about" element={ <About/> }/>
-            <Route path="/contact" element={ <Contact/> }/>
-            {/* <Route path="/protected" element={ <ProtectedPage/> }/> */}
-            {/* <Route path="/controlled" element={ <ControlledFormPage/> }/> */}
-            {/* <Route path="/uncontrolled" element={ <UncontrolledFormPage/> }/> */}
-            {/* <Route path="*" element={ <NotFoundPage/> }/> */}
-      </Routes>
-    </BrowserRouter>
-    </div>
-  );
-}
+	return (
+		<div className="App">
+			<BrowserRouter>
+				<IonPage>
+					<IonContent>
+						<Navbar />
+						<Routes>
+							<Route path="/" element={<Home />} />
+							<Route path="/about" element={<About />} />
+							<Route path="/contact" element={<Contact />} />
+							{/* <Route path="/protected" element={ <ProtectedPage/> }/> */}
+							{/* <Route path="/controlled" element={ <ControlledFormPage/> }/> */}
+							{/* <Route path="/uncontrolled" element={ <UncontrolledFormPage/> }/> */}
+							{/* <Route path="*" element={ <NotFoundPage/> }/> */}
+						</Routes>
+					</IonContent>
+				</IonPage>
+			</BrowserRouter>
+		</div>
+	);
+};
 export default App;
-
-/* 
-  <header id="mainContent"></header>
-  <body>
-  </body>
-  <a id="mainContentLink" href="#mainContent" className="skipLink">main content</a> */
-
-  
